180e0d0523fc08efac8c24bfacaad2454e5f0228
Safraz Doolan <safrazd@gmail.com>
2017-03-23 01:26:03 -0400

    removed

--- application/models/User.php
+++ application/models/User.php
@@ -1,14 +1,3 @@
 <?php 
-   class User extends CI_Model {
-	
-      function __construct() { 
-         parent::__construct(); 
-      } 
-	  
-	   public function insert($data) { 
-         if ($this->db->insert("users", $data)) { 
-            return true; 
-         } 
-      } 
-}
+   
 ?> 
