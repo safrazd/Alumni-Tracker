40988b63f2faa96b4d4a5cf5f5b37fbc10546a13
Safraz Doolan <safrazd@gmail.com>
2017-04-02 14:59:35 -0400

    reorganised files in view and copied css, image and js folders to root

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Register extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,10 +18,10 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index() //function to load login page
+	public function index()
 	{
 		$this->load->helper('url');
-		$this->load->view('login');
+		$this->load->view('registration');
 	}
 	
 }
