29cf8341ee8d914d422608dbd5e39ebb6ee01aa1
darnell <darnellbr665@outlook.com>
2017-04-18 22:01:30 -0400

    update
    updated profiles,viewprofiles

--- darnb/profiles.php
+++ darnb/profiles.php
@@ -1,4 +1,154 @@
 <?php
 $alumniId = $_GET['alumniId'];
-echo $alumniId;
+require("company_info.php");
+require("dbconnect.php");
+$sql="SELECT *
+ FROM ((alumni
+ LEFT JOIN company ON alumni.alumniId=company.alumniId)
+ LEFT JOIN degree ON alumni.alumniId=degree.alumniId)
+ WHERE alumni.alumniId='$alumniId';";
+
+ $res=$con->query($sql);
+
+
+if($res){
+    $row=$res->fetch_assoc();
+echo"<nav class=\"navbar navbar-inverse navbar-default\">
+      <div class=\"container\">
+        <div class=\"navbar-header\">
+          <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">
+            <span class=\"sr-only\">Toggle navigation</span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+          </button>
+          <a class=\"navbar-brand\"><img id =\"webnav\" src=\"logo-uwi.png\" alt=\"\"></a>
+        </div>
+        <div id=\"navbar\" class=\"collapse navbar-collapse\">
+          <ul class=\"nav navbar-nav\">
+            <li><a href=\"wel.html\">Home</a></li>
+            <li class = \"active\"><a href=\"index.html\">About</a></li>
+            <li><a href=\"contact.html\">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+<div class=\"container\">
+    <div class=\"row profile\">
+		<div class=\"col-md-3\">
+			<div class=\"profile-sidebar\">
+				<!-- SIDEBAR USERPIC -->
+				<div class=\"profile-userpic\">
+					<img src=\"profile.jpg\" class=\"img-responsive\" alt=\"\">
+				</div>
+				<!-- END SIDEBAR USERPIC -->
+				<!-- SIDEBAR USER TITLE -->
+				<div class=\"profile-usertitle\">
+					<div class=\"profile-usertitle-name\">
+						".$row["fName"]." ".$row["lName"]."
+					</div>
+					<div class=\"profile-usertitle-job\">
+						Developer
+					</div>
+				</div>
+				<!-- END SIDEBAR USER TITLE -->
+				<!-- SIDEBAR BUTTONS -->
+				<div class=\"profile-userbuttons\">
+					<button type=\"button\" class=\"btn btn-success btn-sm\">Follow</button>
+					<button type=\"button\" class=\"btn btn-danger btn-sm\">Message</button>
+				</div>
+				<!-- END SIDEBAR BUTTONS -->
+				<!-- SIDEBAR MENU -->
+				<div class=\"profile-usermenu\">
+					<ul class=\"nav tab\">
+						<li class=\"active\">
+							<a href=\"#tab1\" data-toggle=\"tab\">
+							<i class=\"glyphicon glyphicon-home\"></i>
+							Overview </a>
+						</li>
+						<li>
+							<a href=\"#tab3\" data-toggle=\"tab\">
+							<i class=\"glyphicon glyphicon-ok\"></i>
+							About Me </a>
+						</li>
+						<li id=\"company_tab\">
+							<a href=\"#tab4\" data-toggle=\"tab\">
+							<i class=\"glyphicon glyphicon-flag\"></i>
+							About My Company </a>
+						</li>
+					</ul>
+				</div>
+				<!-- END MENU -->
+			</div>
+		</div>
+		<div class=\"col-md-9\">
+            <div class=\"profile-content\">
+				<div class=\"tab-content\">
+					<div class=\"tab-pane active text-style\" id=\"tab1\">
+						<form>
+						<div class=\"form-group\">
+							<textarea class=\"form-control\" rows=\"5\" id=\"comment\" placeholder=\"Enter some interest...\"></textarea>
+							<div class=\"row\" id=\"post_button\">
+								<div class=\"col-md-4\"> </div>
+								<div class=\"col-md-4 text-center\"> 
+									<button type=\"button\" class=\"btn btn-primary \">Post</button>	
+								</div>
+								<div class=\"col-md-4\"> </div>
+							</div>
+						</div>
+						</form>		
+					</div>
+						
+					
+					<div class=\"tab-pane text-style\" id=\"tab3\">
+
+							<div class=\"row \">
+                            <div class=\"col-md-6\"><h4>Occupation </h4>
+							<p class=\"occupation\"></p>
+							</div>
+                            <div class=\"col-md-6\"><h4>Degree</h4>
+                                <p class=\"degree\">".$row["degreeName"]."</p>
+							</div>
+                                
+                        </div>
+                        <div class=\"row profileinfo\">
+                            <div class=\"col-md-6\"><h4>Class of Degree awarded</h4>
+							<p class=\"degree_class\">".$row["degreeClass"]."</p>
+						</div>
+                            <div class=\"col-md-6\"><h4>Year Graduated</h4>
+							<p class=\"graduate\">".$row["yearGrad"]."</p>
+							</div>
+                        </div>
+                        <div class=\"row profileinfo\">
+                            <div class=\"col-md-6\"><h4>Degree Acquired </h4>
+								<p class=\"degree_acquired\"></p>
+							</div>
+							<div class=\"col-md-6\"><h4>Email </h4>
+							<p class=\"email\">".$row["email"]."</p>
+							</div>
+                        </div>
+                        <div class=\"row profileinfo\">
+							<div class=\"col-md-6\"><h4>Contact Number</h4>
+                                <p class=\"phone_number\" ></p>
+							</div>
+							<div class=\"col-md-6\"><h4>Country of Birth </h4>
+							<p class=\"birth\"></p>
+							</div>
+                        </div>  
+						</form> 
+					</div>
+                    ";
+						company_info();
+					echo"
+					</div>
+				</div>
+            </div>
+		</div>
+	</div>
+</div>
+";
+}else{
+    echo"error";
+}
 ?>
---
+++ darnb/viewprofile.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<?php session_start();?>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+	 <link href='style.css' rel='stylesheet' type='text/css'>
+	 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    
+     <!-- Latest compiled and minified CSS -->
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+     
+	 <link href='style.css' rel='stylesheet' type='text/css'>
+	 <script src="prof.js"></script>
+	<meta charset="UTF-8">
+	 <link rel="shortcut icon" href="assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    <title>Document</title>
+		
+</head>
+<body style="background-color: #F1F3FA;">
+    <!--
+User Profile Sidebar by @keenthemes
+A component of Metronic Theme - #1 Selling Bootstrap 3 Admin Theme in Themeforest: http://j.mp/metronictheme
+Licensed under MIT
+-->
+<!-- Top menu -->
+<?php
+	require("profiles.php");
+	?>
+<br>
+<br>
+</body>
+</html>
