84c7df4176522baffc9c5b3d2fac3c3b6ec240b7
Safraz Doolan <safrazd@gmail.com>
2017-04-20 02:03:39 -0400

    updated excel

--- application/models/Excel.php
+++ application/models/Excel.php
@@ -8,14 +8,17 @@ Class Excel extends CI_Model //checks if user logging in
          $this->load->database(); 
       } 
 
-	function export(){
+	  function export(){
 if(isset($_POST["export"])){
-
     header('Content-Type: text/csv; charset=utf-8');
-    header('Content-Disposition: attachment; filename=datam.csv');
+    header('Content-Disposition: attachment; filename=All Records.csv');
     $output= fopen("php://output", "w");
-    fputcsv($output, array('AlumniId', 'First Name', 'Last Name', 'Email Address', 'Password', 'Time Created', 'Verified'));
-    $query = "SELECT * FROM `alumni`";
+    fputcsv($output, array('AlumniId', 'First Name', 'Last Name', 'Email Address', 'Company Name', 'Country of Company', 'Occupation', 'Degree Name', 'Degree Class', 'Year Graduated', 'FaceBook', 'LinkedIn', 'Contact Number', 'Country of Birth'));
+    $query = "SELECT a1.alumniId, a1.fName, a1.lName, a1.email, c1.companyName, c1.country, c1.occupation, d1.degreeName, d1.degreeClass, d1.yearGrad, l1.facebook, l1.linkedin, p1.contactNo, p1.birthCountry from `alumni` a1
+              INNER JOIN company c1 ON a1.alumniId=c1.alumniId
+              INNER JOIN degree d1 ON a1.alumniId=d1.alumniId
+              INNER JOIN link l1 ON a1.alumniId=l1.alumniId
+              INNER JOIN personal p1 ON a1.alumniId=p1.alumniId";
     $res = $this->db->query($query);
 
     foreach ($res->result_array() as $row){
@@ -24,7 +27,33 @@ if(isset($_POST["export"])){
     fclose($output);
 }
 
-	}
-}
+if(isset($_POST["export2"])){
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename=Selected Records.csv');
+    $output= fopen("php://output", "w");
+    fputcsv($output, array('AlumniId', 'First Name', 'Last Name', 'Email Address', 'Company Name', 'Country of Company', 'Occupation', 'Degree Name', 'Degree Class', 'Year Graduated', 'FaceBook', 'LinkedIn' ,'Contact Number', 'Country of Birth'));
+    
+    $ids = $_POST['check'];
 
+
+    array_walk($ids, function(&$value, $key){
+        $value =(int)$value;
+    });
+
+    $ids = implode(', ', $ids);
+    
+    $query = "SELECT a1.alumniId, a1.fName, a1.lName, a1.email, c1.companyName, c1.country, c1.occupation, d1.degreeName, d1.degreeClass, d1.yearGrad, l1.facebook, l1.linkedin, p1.contactNo, p1.birthCountry from `alumni` a1
+              INNER JOIN company c1 ON a1.alumniId=c1.alumniId
+              INNER JOIN degree d1 ON a1.alumniId=d1.alumniId
+              INNER JOIN link l1 ON a1.alumniId=l1.alumniId
+              INNER JOIN personal p1 ON a1.alumniId=p1.alumniId WHERE a1.alumniId IN ($ids)";
+    $res = $this->db->query($query);
+
+    foreach ($res->result_array() as $row){
+        fputcsv($output, $row);
+    }
+    fclose($output);
+}
+}
+}
 ?>
