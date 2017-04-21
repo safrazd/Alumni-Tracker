4da9c4782f36959c846b89d6682e0f3c1b1d240c
Safraz Doolan <safrazd@gmail.com>
2017-04-21 10:03:18 -0400

    changed admin and login pages

--- application/views/alumni_login.php
+++ application/views/alumni_login.php
@@ -9,9 +9,11 @@
   <link rel="icon" href="../../favicon.ico">
   <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/style.css">
   <script type = 'text/javascript' src = "<?php echo base_url();?>js/prefixfree.min.js"></script>
-  <script type = 'text/javascript' src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
-  
-    <style>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href='<?php echo base_url(); ?>css/nav.css' rel='stylesheet' type='text/css'>
+	<style>
 
 	#sub{
 	width: 260px;
@@ -29,25 +31,36 @@
 	}
 
 	</style>
-	
+	 <?php
+	 echo"<nav class=\"navbar navbar-inverse navbar-default\">
+      <div class=\"container\">
+        <div class=\"navbar-header\">
+          <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">
+            <span class=\"sr-only\">Toggle navigation</span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+          </button>
+          <a><img src=\" ".base_url()."images/logo1.png\" width='100px' height='100px'  alt=\"\"></a>
+        </div>
+        <div id=\"navbar\" class=\"collapse navbar-collapse\" >
+		  <ul  class=\"nav navbar-nav\" id='pad1' style='position:relative;float:right;'>
+		  <li><a href='".site_url('login/admin')."'>Admin</a></li>
+		  <ul> 
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>";
+	?>
 </head>
 
 <body style="background-color: #F1F3FA;">
-		<div class ="search">
-			<input type="button" value="Admin" onclick="window.location ='<?php echo site_url('login/admin')?>'">
-		</div>
-		<br>
-		<div class = "head">
-			<div><img src="<?php echo base_url('images/logo.png'); ?>" alt="" width="50%" height="50%"></div>
-		</div>
    <?php echo validation_errors(); ?>
    <?php echo form_open('alumniverify'); ?>
-		<div class="login">
+		<div class="login" style="position:absolute; top:150px;">
+				<label style= "font-family: Garamond;font-size:24px;">WELCOME ALUMNI</label>
 				<input type="text" name="email" placeholder="Email" value=""<br>
 				<input type="password" name="password" placeholder="Password" value = ""><br>
 				<input type="submit" id="sub" value="Login">
-				<p>------------------------------ OR -----------------------------</p>
-		
 				<input type="button" onclick="window.location ='<?php echo site_url('welcome/register')?>'" value="Register">
 				
 		</div>
