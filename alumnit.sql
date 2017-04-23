54b6c2d1600832174e7fb5d1a196f442d774dabc
Safraz Doolan <safrazd@gmail.com>
2017-04-23 00:41:37 -0400

    updated database

--- alumnit.sql
+++ alumnit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2017 at 06:31 AM
+-- Generation Time: Apr 23, 2017 at 06:40 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
